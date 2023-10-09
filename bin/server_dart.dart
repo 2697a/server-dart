0cb5e9b13b08b3018a8e7169508708995ef5e711
2697a <972091604@qq.com>
2023-10-09 17:31:26 +0800

    init

---
+++ bin/server_dart.dart
@@ -0,0 +1,5 @@
+import 'package:server_dart/server_dart.dart' as server_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_dart.calculate()}!');
+}
---
+++ test/server_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_dart/server_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
